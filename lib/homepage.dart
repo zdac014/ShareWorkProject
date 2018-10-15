1d203e8495478c256d04692ee006d334b89e257a
Jonathan Lewin <zdac014@live.rhul.ac.uk>
2018-10-15 17:40:32 +0100

    Added text in the middle of the homepage

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -3,6 +3,16 @@ import 'package:flutter/material.dart';
 class Homepage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-      return new Scaffold();
+      return new Scaffold(
+        body: new Container(
+          padding: new EdgeInsets.symmetric(horizontal: 130.0),
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Text('Upload your work here!'),
+            ],
+          ),
+        ),
+      );
     }
 }
